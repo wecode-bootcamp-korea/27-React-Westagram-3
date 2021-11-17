2a29e1453b32b4c63fca3fc224168b667a325cb3
eojine94 <eojine294@gmail.com>
2021-11-17 15:24:40 +0900

    MODIFY : initialized setting modified again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
+import '../src/styles/common.scss';
+import '../src/styles/reset.scss';
+import '../src/styles/variables.scss';
 
 ReactDOM.render(
   <React.StrictMode>

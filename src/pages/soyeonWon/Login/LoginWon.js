fd7700ddf73d9596bd74078d0698bb6c11c44291
dnjstd <dnjstd@gmail.com>
2021-11-17 17:27:39 +0900

    [Add] Add all files

--- src/pages/soyeonWon/Login/LoginWon.js
+++ src/pages/soyeonWon/Login/LoginWon.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginWon.scss';
 
-function LoginWon() {
-  return <div />;
+function Login() {
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main-won');
+  };
+
+  return (
+    <div className="login">
+      <article className="article">
+        <div className="container">
+          <header className="header">
+            <div className="logo">westagram</div>
+          </header>
+
+          <div className="inputBtn">
+            <input
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              id="inputId"
+            />
+            <input type="text" placeholder="비밀번호" id="inputPw" />
+
+            <div>
+              <button className="button" onClick={goToMain}>
+                로그인
+              </button>
+            </div>
+          </div>
+
+          <footer className="footer">
+            <a href="/">비밀번호를 잊으셨나요?</a>
+          </footer>
+        </div>
+      </article>
+    </div>
+  );
 }
 
-export default LoginWon;
+export default Login;
--- src/pages/soyeonWon/Main/MainWon.js
+++ src/pages/soyeonWon/Main/MainWon.js
@@ -1,7 +1,219 @@
 import React from 'react';
+import './MainWon.scss';
 
-function MainWon() {
-  return <div />;
+function Main() {
+  return (
+    <body className="main">
+      <nav className="header">
+        <div className="logo">
+          <img src="/images/soyeonWon/instagram.png" alt="인스타그램 로고" />
+          <span>westagram</span>
+        </div>
+
+        <div className="searchBox">
+          <input type="text" placeholder="검색" />
+          <img src="/images/soyeonWon/search.png" alt="검색아이콘" />
+        </div>
+
+        <div className="icon">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+            alt=""
+          />
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+            alt=""
+          />
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+            alt=""
+          />
+        </div>
+      </nav>
+
+      <main class="mainWrapper">
+        <div className="feeds">
+          <article className="article">
+            <div className="profileBox">
+              <div class="profileImgName">
+                <img
+                  src="/images/soyeonWon/kk.jpg"
+                  alt=""
+                  className="profileImg"
+                />
+                <div className="username">soyeonwon</div>
+              </div>
+              <img
+                src="/images/soyeonWon/option.png"
+                alt="옵션"
+                class="optionIcon"
+              />
+            </div>
+
+            <img src="/images/soyeonWon/kk.jpg" alt="" className="feedImage" />
+            <section className="feedIcon">
+              <div>
+                <img
+                  src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                  alt="좋아요 아이콘"
+                />
+                <img src="/images/soyeonWon/bubble.png" alt="댓글 아이콘" />
+                <img src="/images/soyeonWon/send.png" alt="메세지 아이콘" />
+              </div>
+              <img src="/images/soyeonWon/save.png" alt="보관 아이콘" />
+            </section>
+            <section className="feedLike">
+              <img
+                src="/images/soyeonWon/img01.png"
+                alt=""
+                className="likeProfileImg"
+              />
+              <span>wecode_bootcamp</span>
+              <span>님&nbsp;</span>
+              <span>외 4명</span>
+              <span>이 좋아합니다</span>
+            </section>
+            <section className="contentsBox">
+              <span className="username">soyeonwon</span>
+              <span className="contentsText">귀요미!!!</span>
+            </section>
+            <ul className="commentList" />
+            <div className="commentTime">54분 전</div>
+            <form className="commentBox">
+              <input
+                className="commentInput"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button className="commentBtn">게시</button>
+            </form>
+          </article>
+        </div>
+        <div className="mainRight">
+          <div className="profileBoxAside">
+            <img
+              src="/images/soyeonWon/img01.png"
+              alt=""
+              className="profileBoxAsideImg"
+            />
+            <div className="profile">
+              <div className="username">wecode_bootcamp</div>
+              <div className="text">weCode | 위코드</div>
+            </div>
+          </div>
+
+          <div className="storyBox">
+            <div className="mainRightTitle">
+              <span>스토리</span>
+              <button>모두 보기</button>
+            </div>
+
+            <div className="storyContents">
+              <div className="profileBoxStory">
+                <img
+                  src="/images/soyeonWon/img02.jpg"
+                  alt=""
+                  className="StoryImg"
+                />
+                <div>
+                  <div className="username">wecode_bootcamp</div>
+                  <div className="text">16분 전</div>
+                </div>
+              </div>
+
+              <div className="profileBoxStory">
+                <img
+                  src="/images/soyeonWon/img03.jpg"
+                  alt=""
+                  className="StoryImg"
+                />
+                <div>
+                  <div className="username">wecode_bootcamp</div>
+                  <div className="text">3시간 전</div>
+                </div>
+              </div>
+
+              <div className="profileBoxStory">
+                <img
+                  src="/images/soyeonWon/img04.jpg"
+                  alt=""
+                  className="StoryImg"
+                />
+                <div>
+                  <div className="username">wecode_bootcamp</div>
+                  <div className="text">20시간 전</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="recommendBox">
+            <div className="mainRightTitle">
+              <span>회원님을 위한 추천</span>
+              <button>모두 보기</button>
+            </div>
+
+            <div className="profileBoxRecommend">
+              <img
+                src="/images/soyeonWon/img05.jpg"
+                alt=""
+                className="profileBoxRecoImg"
+              />
+              <div>
+                <div className="username">wecode_bootcamp</div>
+                <div className="text">wecode님 외 12명이 팔로우 ...</div>
+              </div>
+              <button>팔로우</button>
+            </div>
+
+            <div className="profileBoxRecommend">
+              <img
+                src="/images/soyeonWon/img06.jpg"
+                alt=""
+                className="profileBoxRecoImg"
+              />
+              <div>
+                <div className="username">wecode_bootcamp</div>
+                <div className="text">wecode_bootcamp님 외 4 ...</div>
+              </div>
+              <button>팔로우</button>
+            </div>
+
+            <div className="profileBoxRecommend">
+              <img
+                src="/images/soyeonWon/img07.jpg"
+                alt=""
+                className="profileBoxRecoImg"
+              />
+              <div>
+                <div className="username">wecode_bootcamp</div>
+                <div className="text">wecode님 외 7명이 팔로우 ...</div>
+              </div>
+              <button>팔로우</button>
+            </div>
+          </div>
+
+          <div className="corpArea">
+            <ul>
+              <li>INSTAGRAM 정보</li>
+              <li>지원</li>
+              <li>홍보센터</li>
+              <li>API</li>
+              <li>채용 정보</li>
+              <li>개인정보처리방침</li>
+              <li>약관</li>
+              <li>디렉터리</li>
+              <li>프로필</li>
+              <li>해시태그</li>
+              <li>언어</li>
+            </ul>
+
+            <span>© 2021 INSTAGRAM</span>
+          </div>
+        </div>
+      </main>
+    </body>
+  );
 }
 
-export default MainWon;
+export default Main;

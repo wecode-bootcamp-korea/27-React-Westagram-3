61248fa9ab06076c1118ce47e48eee0c22957cfb
성현Maria 김 <globalkmaria@gmail.com>
2021-11-23 20:28:55 +0900

    Refactor: footer info 부분 상수 데이터로 바꿈

--- src/pages/sunghyunKim/Main/Footer/Footer.js
+++ src/pages/sunghyunKim/Main/Footer/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { INFO_LIST } from '../../../../data/footData';
 import './Footer.scss';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="instagramInfo">
-        Instagram 정보 &#183; 지원 &#183; 홍보 센터 &#183; API &#183; 채용 정보
-        &#183; 개인정보처리방침 &#183; 약관 &#183; 디렉터리 &#183; 프로필 &#183;
-        해시태그 &#183; 언어
-      </p>
+      <ul>
+        {INFO_LIST.map(info => {
+          const { id, content } = info;
+          return (
+            <li key={id}>
+              <a href="#">{content}</a>
+            </li>
+          );
+        })}
+      </ul>
       <span className="copyright"> © 2019 INSTAGRAM </span>
     </footer>
   );
